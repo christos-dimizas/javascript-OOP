53a5968f6e337187c0cc310b703964da6f3e0a42
Christos Dimizas <christos.dimizas@marinetraffic.com>
2018-07-16 13:00:11 +0300

    OOP Javascript - Prototypes

--- course_sections/stopWatch.js
+++ course_sections/stopWatch.js
@@ -1,33 +1,49 @@
-
+// Refactor using prototypes.
 function StopWatch() {
     let startTime, endTime, running, duration = 0;
 
-    this.start = () => {
-        if (running) {
-            throw new Error('StopWatch has already started.');
-        }
-        running = true;
-        startTime = new Date();
-    };
-
-    this.stop = () => {
-        if (!running) {
-            throw new Error('StopWatch is not started.');
-        }
-        running = false;
-        endTime = new Date();
-        const secs = (endTime.getTime() - startTime.getTime()) / 1000;
-        duration += secs
-    };
-
-    this.reset = () => {
-        duration = 0;
-        startTime = null;
-        endTime = null;
-        running = false;
-    };
-
     Object.defineProperty(this, 'duration', {
-        get: () => duration
+        get: () => duration,
+        set: (value) => duration = value
+    });
+
+    Object.defineProperty(this, 'startTime', {
+        get: () => startTime
+    });
+
+    Object.defineProperty(this, 'endTime', {
+        get: () => endTime
+    });
+
+    Object.defineProperty(this, 'running', {
+        get: () => running
     });
 }
+
+StopWatch.prototype.start = () => {
+    if (this.running) {
+        throw new Error('StopWatch has already started.');
+    }
+    this.running = true;
+    this.startTime = new Date();
+};
+
+StopWatch.prototype.stop = () => {
+    if (!this.running) {
+        throw new Error('StopWatch is not started.');
+    }
+    this.running = false;
+    this.endTime = new Date();
+    this.duration += (this.endTime.getTime() - this.startTime.getTime()) / 1000;
+};
+
+StopWatch.prototype.reset = () => {
+    this.duration = 0;
+    this.startTime = null;
+    this.endTime = null;
+    this.running = false;
+};
+
+
+// TODO - BY ADDING A SETTER TO DURATION WE BROKE OUR APP BECAUSE NOW USER CAN
+// TODO - MODIFY THE DURATION WHICH LEADS TO FAULTY RESULTS.
